d9139ec1867dac8d9c2ca7698feea228d1c24efc
Chaowei Zeng <sli616@163.com>
2017-09-30 01:39:59 +0800

    simplify index view, import feature and details pages

---
+++ page/detail/detail.js
@@ -0,0 +1,33 @@
+// 获取全局应用程序实例对象
+const app = getApp();
+
+// 创建页面实例对象
+Page({
+  name: "detail",
+
+  data: {},
+
+  onLoad() {
+
+  },
+
+  onReady() {
+
+  },
+
+  onShow() {
+  },
+
+  onHide() {
+
+  },
+
+  onUnload() {
+
+  },
+
+  onPullDownRefresh() {
+
+  },
+
+});
---
+++ page/feature/feature.js
@@ -0,0 +1,49 @@
+
+
+// 获取全局应用程序实例对象
+const app = getApp();
+
+// 创建页面实例对象
+Page({
+  name: "feature",
+
+  data: {
+    
+  
+  
+  },
+
+  onLoad () {
+
+  },
+
+  onReady () {
+
+  },
+
+  onShow () {
+  },
+
+  onHide () {
+
+  },
+
+  onUnload () {
+
+  },
+
+  onPullDownRefresh () {
+    
+  },
+
+
+  //以下为自定义点击事件
+  
+  tap_d9c62767:function(e){
+  },
+  
+  tap_de01fdb6:function(e){
+  },
+  
+})
+
